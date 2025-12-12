b4cc13e183d0214f5ff7a3714808272611102832
garmatyair-design <garmat.yair@gmail.com>
2025-12-12 17:09:40 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /* ================= CONFIG SUPABASE ================= */
 const SUPABASE_URL = "https://imhoqcsefymrnpqrhvis.supabase.co";
-const SUPABASE_ANON_KEY = "PEGA_AQUI_TU_ANON_KEY";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImltaG9xY3NlZnltcm5wcXJodmlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU0OTY5ODIsImV4cCI6MjA4MTA3Mjk4Mn0.jplAkiMPXl6V5KT4P9h3OXAJNOwSsF9ZVz6nVIo6a9A";
 
 const supabase = window.supabase.createClient(
   SUPABASE_URL,
@@ -81,4 +81,3 @@ if($("btnProcesarDocs")){
     location.reload();
   };
 }
-
